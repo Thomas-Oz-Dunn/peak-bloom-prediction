fc17c83913a6500d3e03aba7fcfbcf40cea8c12f
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2022-02-16 21:25:59 -0600

    Implementing normalization and fft of data sets

--- Cherry Blossom Prediction.r
+++ Cherry Blossom Prediction.r
@@ -118,12 +118,6 @@ historic_min_temperatures <-
   bind_rows(tibble(location = "kyoto", get_min_temperature("JA000047759"))) %>%
   bind_rows(tibble(location = "vancouver", get_min_temperature("CA001108395")))
 
-ls_fit_max_temperature <- lm(tmax_avg ~ year * season + location, 
-                            data = historic_max_temperatures)
-ls_fit_min_temperature <- lm(tmin_avg ~ year * season + location, 
-                            data = historic_min_temperatures)
-# fft()?
-
 # Precipitaiton Model
 historic_precipitation <-
   tibble(location = "washingtondc", get_precipitation("USC00186350")) %>%
@@ -137,14 +131,6 @@ historic_snowfall <-
   bind_rows(tibble(location = "kyoto", get_snowfall("JA000047759"))) %>%
   bind_rows(tibble(location = "vancouver", get_snowfall("CA001108395")))
 
-ls_fit_precipitation <- lm(prec_avg ~ year * month * day + location, 
-                            data = historic_precipitation)
-
-ls_fit_snowfall <- lm(snow_avg ~ year * month * day + location, 
-                            data = historic_snowfall)
-                            
-# fft()
-
 # Solar Irradiance Model
 historic_sunshine <-
   tibble(location = "washingtondc", get_sunshine"USC00186350")) %>%
@@ -152,20 +138,47 @@ historic_sunshine <-
   bind_rows(tibble(location = "kyoto", get_sunshine("JA000047759"))) %>%
   bind_rows(tibble(location = "vancouver", get_sunshine("CA001108395")))
 
-ls_fit_sunshine <- lm(sun_avg ~ year * month * day + location, 
-                            data = historic_sunshine)
-# fft()?
-
 # Plan:
 # Run least square fit models on each for linear approximations
+ls_fit_max_temperature <- lm(tmax_avg ~ year * season + location, 
+                            data = historic_max_temperatures)
+ls_fit_min_temperature <- lm(tmin_avg ~ year * season + location, 
+                            data = historic_min_temperatures)
+ls_fit_precipitation <- lm(prec_avg ~ year * month * day + location, 
+                            data = historic_precipitation)
+ls_fit_snowfall <- lm(snow_avg ~ year * month * day + location, 
+                            data = historic_snowfall)
+ls_fit_sunshine <- lm(sun_avg ~ year * month * day + location, 
+                            data = historic_sunshine)
+
 # Subtract linear value (time) form each set to flatten before fft
 # Divide entire dataset by 1/2*(max - min)
+normalized_max_temperature <- 2*(historic_max_temperatures - ls_fit_max_temperature) /
+                        (max(historic_max_temperatures) - min(historic_max_temperatures))
+normalized_min_temperature <- 2*(historic_min_temperatures - ls_fit_min_temperature) /
+                        (max(historic_min_temperatures) - min(historic_min_temperatures))
+normalized_precipitation <- 2*(historic_precipitation - ls_fit_precipitation) /
+                        (max(historic_precipitation) - min(historic_precipitation))
+normalized_snowfall <- 2*(historic_snowfall - ls_fit_snowfall) /
+                        (max(historic_snowfall) - min(historic_snowfall))
+normalized_sunshine <- 2*(historic_sunshine - ls_fit_sunshine) /
+                        (max(historic_sunshine) - min(historic_sunshine))
+
 # Perform fft on normalized data to run frequency analysis (find major cycles)
-# Filter out all values of slower frequency than the collection
+max_temp_freqs <- fft(normalized_max_temperature)
+min_temp_freqs <- fft(normalized_min_temperature)
+prec_freqs <- fft(normalized_precipitation)
+snowfall_freqs <- fft(normalized_snowfall)
+sunshine_freqs <- fft(normalized_sunshine)
+
+# Filter out all values of slower frequency than the collection length
 # Filter out all value of faster frequency than daily
 # Noise floor, keep all with abs() > x dB
 # Set annual as "zero" frequency
+
+
 # InvFFT back to time series data, what remains is "ahead" and "behind" yearly cycle (phase)
 # Find covariance factor with each variable with bloom day "ahead" and "behind" linear <- (subtract ls linear fit yo find remainung phase offset)
 # Synthesize all models based on relative covariance weighting for training data
 # Compare predictive errors rates on testing data
+# Simple perceptron?
