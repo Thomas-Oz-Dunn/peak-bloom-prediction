ec63f03ae66ccd572a0a484041eb79b8cec285ae
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2022-02-17 20:30:37 -0600

    Began testing, worked up until normalization, needs refactoring

--- Cherry Blossom Prediction.r
+++ Cherry Blossom Prediction.r
@@ -100,7 +100,7 @@ cherry <- read.csv("data/washingtondc.csv") %>%
   bind_rows(read.csv("data/liestal.csv")) %>%
   bind_rows(read.csv("data/kyoto.csv")) %>%
   bind_rows(read.csv("data/meteoswiss.csv")) %>%
-  bind_rows(read.csv("data/south_korea.csv")) %>%
+  bind_rows(read.csv("data/south_korea.csv"))
 
 # First Order fit
 ls_fit <- lm(bloom_doy ~ location * year, data = cherry, subset = year >= 1880)
@@ -153,6 +153,14 @@ ls_fit_sunshine <- lm(sun_avg ~ year * month * day + location,
 
 # Subtract linear value (time) form each set to flatten before fft
 # Divide entire dataset by 1/2*(max - min)
+min_max_norm <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
+
+norm_max_temp <-as.data.frame(lapply(historix_max_temperatures, min_max_norm))
+
+
+
 normalized_max_temperature <- 2*(historic_max_temperatures - ls_fit_max_temperature) /
                         (max(historic_max_temperatures) - min(historic_max_temperatures))
 normalized_min_temperature <- 2*(historic_min_temperatures - ls_fit_min_temperature) /
@@ -171,14 +179,26 @@ prec_freqs <- fft(normalized_precipitation)
 snowfall_freqs <- fft(normalized_snowfall)
 sunshine_freqs <- fft(normalized_sunshine)
 
+freq_series <- cbind(0:(length(max_temp_freqs)-1), Mod(max_temp_freqs))
+freq_series[2:length(max_temp_freqs),2] <- 2*freq_series[2:length(max_temp_freqs),2] 
+plot(freq_series, 
+      t="h", 
+      lwd=2, 
+      main = "", 
+      xlab = "Frequency", 
+      ylab = "Amplitude",
+      xlim = c(0,length(max_temp_freqs))
+      ylim = c(0,max(Mod(freq_series[,2]))))
+      
 # Filter out all values of slower frequency than the collection length
 # Filter out all value of faster frequency than daily
 # Noise floor, keep all with abs() > x dB
 # Set annual as "zero" frequency
 
+# Phase offset in days = sin-1(func(day)) - day
 
 # InvFFT back to time series data, what remains is "ahead" and "behind" yearly cycle (phase)
-# Find covariance factor with each variable with bloom day "ahead" and "behind" linear <- (subtract ls linear fit yo find remainung phase offset)
+# Find covariance factor with each variable with bloom day "ahead" and "behind" linear (dotproduct)
 # Synthesize all models based on relative covariance weighting for training data
 # Compare predictive errors rates on testing data
 # Simple perceptron?
